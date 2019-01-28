d5500c054f407811693462431979ec1c4f16b4bf
Peter Tiernan <ptiernan@gmail.com>
2019-01-28 17:26:58 +0000

    mobile nav

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -18,6 +18,8 @@
         <li><a href="/#services">Services <i class="fas fa-arrow-circle-down"></i></a></li>
         <li><a href="/#about">About</a></li>
         <li><a href="/contact">Contact</a></li>
+
+        <button class="menu btn">Menu<i class="fas fa-bars"></i></button>
     </div>
     <div class="nav-divider-3">
         <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 152.81 114"><title>background-1</title><path d="M980.5,641.5h-80c5-79-70-113-70-113h150Z" transform="translate(-828.19 -528)" fill="#f99637" stroke="#f99637" stroke-miterlimit="10"/></svg>
@@ -26,4 +28,9 @@
 
 </div>
 
-
+<div class="dropdown-nav">
+    <li class="dropdown-nav-item"><a href="/" class="dropdown-nav-link">Home</a></li>
+    <li class="dropdown-nav-item"><a href="/#services" class="dropdown-nav-link">Services</a></li>
+    <li class="dropdown-nav-item"><a href="/#about" class="dropdown-nav-link">About</a></li>
+    <li class="dropdown-nav-item"><a href="/contact" class="dropdown-nav-link">Contact</a></li>
+</div>
